d76d828d3294297a6a721c0b680c807b82f52974
zacharyyamaoka <zach.yamaoka@gmail.com>
2017-11-23 21:29:47 +0000

    music

--- project.py
+++ project.py
@@ -11,12 +11,12 @@ input0 =
 input0 = '''
 
 
-trigger0 = 'q'
-trigger1 = 'w'
-trigger2 = 'e'
-trigger3 = 'a'
-trigger4 = 's'
-trigger5 = 'd'
+trigger0 = 'a'
+trigger1 = 's'
+trigger2 = 'd'
+trigger3 = 'f' # good together
+trigger4 = 'g' # good
+trigger5 = 'h'
 trigger6 = 'r'
 trigger7 = 'f'
 trigger8 = 't'
@@ -26,11 +26,11 @@ trigger10 = 'h'
 pg.mixer.init()
 pg.mixer.set_num_channels(10)
 
-s0 = pg.mixer.Sound('edx.wav')
-s1 = pg.mixer.Sound('bass.wav')
-s2 = pg.mixer.Sound('drum1.wav')
-s3 = pg.mixer.Sound('edx.wav')
-s4 = pg.mixer.Sound('edx.wav')
+s0 = pg.mixer.Sound('sub_bass.ogg')
+s1 = pg.mixer.Sound('bass2.ogg')
+s2 = pg.mixer.Sound('drum4.ogg')
+s3 = pg.mixer.Sound('melody2.ogg')
+s4 = pg.mixer.Sound('drum2.ogg')
 s5 = pg.mixer.Sound('edx.wav')
 s6 = pg.mixer.Sound('edx.wav')
 s7 = pg.mixer.Sound('edx.wav')
@@ -48,7 +48,8 @@ channel7 = pg.mixer.Channel(7)
 channel8 = pg.mixer.Channel(8)
 channel9 = pg.mixer.Channel(9)
 
-#channel1.play(s0, loops = -1)
+#channel9.play(s9, loops = -1)
+#channel9.set_volume(0.5)
 print('working')
 while True:
     ball_input = str(input('press a key'))
@@ -59,13 +60,35 @@ while True:
         else:
             channel0.play(s0, loops = -1)
 
+    if ball_input == trigger1:
+        if channel1.get_busy():
+            channel1.stop()
+        else:
+            channel1.play(s1, loops = -1)
+
     if ball_input == trigger2:
         if channel2.get_busy():
             channel2.stop()
         else:
-            channel2.play(s1, loops = -1)
+            channel2.play(s2, loops = -1)
 
+    if ball_input == trigger3:
+        if channel3.get_busy():
+            channel3.stop()
+        else:
+            channel3.play(s3, loops = -1)
 
+    if ball_input == trigger4:
+        if channel4.get_busy():
+            channel4.stop()
+        else:
+            channel4.play(s4, loops = -1)
+
+    if ball_input == trigger5:
+        if channel5.get_busy():
+            channel5.stop()
+        else:
+            channel5.play(s5, loops = -1)
 
     #time.sleep(10)
 
