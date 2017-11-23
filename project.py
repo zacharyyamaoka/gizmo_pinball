0baa2f2f9d5fbdded75869bcce6f45ea3410d226
Zachary Yamaoka <zach.yamaoka@gmail.com>
2017-11-23 19:24:12 +0000

    update

--- project.py
+++ project.py
@@ -1,4 +1,13 @@
-from pydub import AudioSegment
+import pygame as pg
+import random
+import time
 
-print('working')
-song = AudioSegment.from_mp3("ed.mp3")
+pg.mixer.init()
+sound = pg.mixer.Sound('edx.wav')
+channel1 = pg.mixer.Channel(0)
+channel1.play(sound, loops = -1)
+
+while True:
+    time.sleep(10)
+#while mixer.music.get_busy():
+#    time.Clock().tick(10)
